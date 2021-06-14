c0a330dd8fdfe80fd20e9003171080cc19046a5b
JulienTigoulet <live_julien@hotmail.fr>
2021-06-14 15:51:46 +0200

    handler img modification

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -138,6 +138,8 @@ export default {
         formData.append('userUuid', localStorage.getItem('userUuid'));
       }
       else{
+         this.image = this.imageUrl 
+        formData.append('image', this.image)
         formData.append('body', this.newBody)
         formData.append('userUuid', localStorage.getItem('userUuid'))
       }
